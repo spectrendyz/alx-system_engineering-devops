1814fae95dd376748c221adfc4afdfff44ddf3fc
spectrendyz <akpaudoetieneobong@gmail.com>
2022-08-31 04:14:20 -0700

    my shell commit

--- 0x00-shell_basics/0-current_working_directory.sh
+++ 0x00-shell_basics/0-current_working_directory.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-Dir_path = $(cd $(dirname "${BASH_SOURCE:-$0}") && pwd)
-abs_path = Dir_path/‚Å$(basename "${BASH_SOURCE:-$0}¬")
-echo 'the absolute path is' $abs_path
+path = 'readlink -f "${BASH_SOURCE:-$O}"'
+echo 'the absolute path is' ‚$path
